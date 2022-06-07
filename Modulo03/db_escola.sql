66ae31967d0d783e454b6d21c7e71db30887b24c
BRUNO APOSTOLICO <bapostolico@gmail.com>
2022-06-07 12:48:10 -0300

    Chave Primária

--- Modulo03/db_escola.sql
+++ Modulo03/db_escola.sql
@@ -6,30 +6,32 @@ USE db_escola;
 
 -- Criar Tabela --
 CREATE TABLE tb_professor (
-                              nome VARCHAR(100) NOT NULL,
-                              cpf CHAR(11) UNIQUE NOT NULL,
-                              email VARCHAR(255) UNIQUE NOT NULL
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    cpf CHAR(11) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE tb_aluno (
-                          nome VARCHAR(100) NOT NULL,
-                          email VARCHAR(255) UNIQUE NOT NULL,
-                          cpf CHAR(11) UNIQUE NOT NULL,
-                          matricula VARCHAR(10) UNIQUE NOT NULL
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    cpf CHAR(11) UNIQUE NOT NULL,
+    matricula VARCHAR(10) UNIQUE NOT NULL
 );
 
 CREATE TABLE tb_curso (
-                          nome VARCHAR(100) UNIQUE NOT NULL,
-                          descricao VARCHAR(255)
+    nome VARCHAR(100) UNIQUE NOT NULL,
+    descricao VARCHAR(255)
 );
 
 CREATE TABLE tb_disciplina (
-                               nome VARCHAR(100) UNIQUE NOT NULL,
-                               descricao VARCHAR(255)
+    nome VARCHAR(100) UNIQUE NOT NULL,
+    descricao VARCHAR(255)
 );
 
 -- excluir tabela --
-    DROP TABLE tb_aluno;
+    DROP TABLE tb_professor;
 
 -- inserir dados --
     INSERT INTO tb_professor (nome, email, cpf)
