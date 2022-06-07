27e1e82d5d60d24534d15f465cf3ba0510f5d2f8
BRUNO APOSTOLICO <bapostolico@gmail.com>
2022-06-07 19:05:32 -0300

    Cadastro de Produtos em desenvolvimento

--- Modulo03/projeto-final/db/db_store.sql
+++ Modulo03/projeto-final/db/db_store.sql
@@ -28,3 +28,27 @@ INSERT INTO tb_category (name, description) VALUES
 ('Eletrônicos','TVs, Som portátil, Caixas de Som, etc');
 
 SELECT * FROM tb_category;
+
+SHOW TABLES ;
+
+INSERT INTO tb_product (name, description, photo, valor, category_id, quantity, created_at) VALUES
+('PC Desktop','Computador para uso pessoal')
+('Computador gamer','Computador para jogos')
+('Notebook','Computador portátil')
+('Tablet','Computador de mão')
+('All in One','Computador compacto')
+('Disco rígido (HD)','HDs para computadores e Notebooks')
+('Memória RAM','Memória para computadores e notebooks')
+('WebCam','Camera para uso em computadores')
+('Teclado','Teclado de computador')
+('Mouse','Mouse para computador')
+('Papel','Pacote de Papel')
+('Post-its','Adesivos para recados')
+('Caneta','Canetas de cores variadas')
+('Lápis','Lapis para desenho')
+('Caderno','Caderno de Vários tamanhos')
+('TV','Smart TV')
+('Som portátil','Som')
+('Celular','SmartPhone')
+('Relógios Digitais','SmartWatch')
+('Video Game','Diversos Modelos de Vídeo Game')
