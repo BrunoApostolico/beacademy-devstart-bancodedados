a5dbbc054205750b0b4427d210b5d8d29c79e9df
Bruno Apostolico <bapostolico@gmail.com>
2022-06-11 01:39:16 -0300

    Ajuste acentuação utf8

--- Modulo03/projeto-final/src/Connection/Connection.php
+++ Modulo03/projeto-final/src/Connection/Connection.php
@@ -12,6 +12,7 @@ abstract class Connection
         $username = 'root';
         $password = '12345';
 
-        return new \PDO('mysql:host=localhost;dbname='.$database, $username, $password);
+        return new \PDO('mysql:host=localhost;dbname='.$database, $username, $password,
+            array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     }
 }
